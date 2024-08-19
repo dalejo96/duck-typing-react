d4d46cc088afcd6ba34404e747431164973bde4a
dalejo96 <dalejo_1996@hotmail.com>
2024-08-18 23:09:00 -0500

    add fixture with harcoded apps

--- src/fixtures/state.ts
+++ src/fixtures/state.ts
@@ -9,7 +9,7 @@ mapApps.set('App-1', [
   },
   {
     app_id: 'App-1',
-    app_name: 'hello',
+    app_name: 'App 1',
     app_skeleton: {
       actions: [],
       command: {
@@ -18,16 +18,47 @@ mapApps.set('App-1', [
       },
       components: [
         {
-          error: null,
-          id: null,
-          input_actions: [],
-          loading: false,
-          options: [],
-          recommendation: 'Change Me!',
-          text: 'Hello World',
+          id: 'id-000',
+          text: 'Please fill out the following form and tell us a bit about yourself.',
+          type: 'text',
+          variant: 'subtitle1'
+        },
+        {
+          id: 'id-001',
+          text: 'Full name',
           type: 'input',
-          validations: {},
           value: null
+        },
+        {
+          id: 'id-002',
+          text: 'Email',
+          type: 'input',
+          value: null
+        },
+        {
+          id: 'id-003',
+          text: 'Phone number',
+          type: 'input',
+          value: null
+        },
+        {
+          id: 'id-004',
+          text: 'Comments',
+          type: 'textarea',
+          value: null
+        },
+        {
+          id: 'id-005',
+          text: 'I accept the Terms of Policy',
+          type: 'checkbox',
+          value: false
+        },
+        {
+          id: 'id-006',
+          text: 'Submit',
+          type: 'button',
+          actions: [],
+          variant: 'contained'
         }
       ]
     },
@@ -41,7 +72,7 @@ mapApps.set('App-2', [
   },
   {
     app_id: 'App-2',
-    app_name: 'hello',
+    app_name: 'App 2',
     app_skeleton: {
       actions: [],
       command: {
@@ -50,16 +81,23 @@ mapApps.set('App-2', [
       },
       components: [
         {
-          error: null,
-          id: null,
-          input_actions: [],
-          loading: false,
-          options: [],
-          recommendation: 'Change Me!',
-          text: 'Hello World App 2',
+          id: 'id-000',
+          text: 'Please write your name on the input below',
+          type: 'text',
+          variant: 'h6'
+        },
+        {
+          id: 'id-001',
+          text: 'Name',
           type: 'input',
-          validations: {},
           value: null
+        },
+        {
+          id: 'id-006',
+          text: 'Submit',
+          type: 'button',
+          actions: [],
+          variant: 'outlined'
         }
       ]
     },
@@ -73,7 +111,7 @@ mapApps.set('App-3', [
   },
   {
     app_id: 'App-3',
-    app_name: 'hello',
+    app_name: 'App 3',
     app_skeleton: {
       actions: [],
       command: {
@@ -82,16 +120,42 @@ mapApps.set('App-3', [
       },
       components: [
         {
-          error: null,
-          id: null,
-          input_actions: [],
-          loading: false,
-          options: [],
-          recommendation: 'Change Me!',
-          text: 'Hello World App 3',
-          type: 'input',
-          validations: {},
-          value: null
+          id: 'id-000',
+          text: 'Please open the accordion and complete the information',
+          type: 'text',
+          variant: 'h6'
+        },
+        {
+          id: 'id-001',
+          label: 'My cool container',
+          tchildren: [
+            {
+              id: 'id-002',
+              text: 'First name',
+              type: 'input',
+              value: null
+            },
+            {
+              id: 'id-003',
+              text: 'Middle name',
+              type: 'input',
+              value: null
+            },
+            {
+              id: 'id-004',
+              text: 'Last name',
+              type: 'input',
+              value: null
+            },
+            {
+              id: 'id-005',
+              text: 'Action!',
+              type: 'button',
+              actions: [],
+              variant: 'text'
+            }
+          ],
+          type: 'accordion'
         }
       ]
     },
